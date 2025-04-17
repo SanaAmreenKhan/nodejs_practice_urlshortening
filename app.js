ceb1d6907a5935c23ecc17250469a5548ddfda4e
SanaAmreenKhan <sanaamreenkhan9156@gmail.com>
2025-04-17 10:06:52 +0300

    Port related issue resolved.

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ const server = createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => {
-  console.log("server running at https://localhost");
+server.listen(process.env.PORT || PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${process.env.PORT || PORT}`);
 });
